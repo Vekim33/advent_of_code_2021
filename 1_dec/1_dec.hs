079547c24d502671005b5bee6139c4f45c8d2003
Veronica Kimelman <veronicakimelman@hotmail.com>
2021-12-02 17:32:23 +0100

    Part 2

--- 1_dec/1_dec.hs
+++ 1_dec/1_dec.hs
@@ -1,14 +1,25 @@
-main = do  
-    contents <- readFile "input.txt" 
-    putStrLn  . show $ getIncreases contents
-  
-getIncreases :: String -> Int  
+main = do
+    contents <- readFile "input.txt"
+    putStrLn  $ "Number of increases: " ++ show (getIncreases contents)
+     ++ "\n" ++ "Number of three sum increases: " ++ show (getThreeSumIncreases contents)
+
+getIncreases :: String -> Int
 getIncreases input = numberOfIncreases . map read $ lines input
+
+getThreeSumIncreases :: String -> Int
+getThreeSumIncreases input = numberOfIncreases $ threeMeasurementSum $ map read $ lines input
 
 numberOfIncreases :: [Int] -> Int
 numberOfIncreases []  = 0
 numberOfIncreases [x] = 0
 numberOfIncreases (x:xs)
- | x < y = 1 + numberOfIncreases xs
+ | x < y     = 1 + numberOfIncreases xs
  | otherwise = numberOfIncreases xs
  where y = head xs
+
+threeMeasurementSum :: [Int] -> [Int]
+threeMeasurementSum []  = []
+threeMeasurementSum [x] = []
+threeMeasurementSum (x:y:xs)
+ | null xs = []
+ | otherwise = (x + y + head xs) : threeMeasurementSum (y:xs)
